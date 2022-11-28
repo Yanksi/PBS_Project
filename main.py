511736e8d95d32b1e3e5c7cd53a969ddc7611947
Shuhao Li <me@yanksi.li>
2022-11-28 22:01:33 +0100

    press space to start animating

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ scene.set_camera(camera)
 frame_count = 0
 gui = window.get_gui()
 
+animating = False
 while window.running:
     ad = 0.0
     ws = 0.0
@@ -41,14 +42,18 @@ while window.running:
     if window.is_pressed(ti.ui.UP): ws = 1
     elif window.is_pressed(ti.ui.DOWN): ws = -1
 
+    if window.is_pressed(ti.ui.SPACE): animating = True
+
     if particle_grid.dim == 2:
         ext_acc = ti.math.vec2(0, -9.8) + ad * ti.math.vec2(5, 0)
-        # solver.step_solver(ext_acc)
+        if animating:
+            solver.step_solver(ext_acc)
         canvas.set_background_color((0,0,0))
         canvas.circles(particle_grid.particle_field.p, radius=particle_grid.particle_radius, color=particle_grid.particle_field.color)
     else:
         ext_acc = ti.math.vec3(0,0,-9.8) + ad * ti.math.vec3(5,0,0) + ws * ti.math.vec3(0,5,0)
-        # solver.step_solver(ext_acc)
+        if animating:
+            solver.step_solver(ext_acc)
         camera.track_user_inputs(window, movement_speed=0.02, hold_key=ti.ui.LMB)
         scene.set_camera(camera)
 
